8358b2f08be267b3e0bd69d1e4202f28063122bc
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-02-19 00:28:53 +0000

    Add Problems With Ado.Net - RemoveVillain And PrintAllMinionNames

---
+++ ADO.NETExercise/06.RemoveVillain/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.RemoveVillain
+{
+    using System;
+    using Microsoft.Data.SqlClient;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server = .; Integrated Security = true; Database = MinionsDB";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                int value = int.Parse(Console.ReadLine());
+
+                string evilNameQuery = "SELECT Name FROM Villains WHERE Id = @villainId";
+                using var sqlCommand = new SqlCommand(evilNameQuery, connection);
+                sqlCommand.Parameters.AddWithValue("@villainId", value);
+                var name = (string)sqlCommand.ExecuteScalar();
+
+                if (name == null)
+                {
+                    Console.WriteLine("No such villain was found.");
+                    return;
+                }
+
+                var deleteMinionsVillainsQuery = @"DELETE FROM MinionsVillains 
+                                           WHERE VillainId = @villainId";
+
+                using var sqlDeleteMinionsVillainsCommand = new SqlCommand(deleteMinionsVillainsQuery, connection);
+                sqlDeleteMinionsVillainsCommand.Parameters.AddWithValue("@villainId", value);
+                var affectedRows = sqlDeleteMinionsVillainsCommand.ExecuteNonQuery();
+
+                var deleteVillainQuery = @"DELETE FROM Villains
+                                           WHERE Id = @villainId";
+                using var sqlDeleteVillainsCommand = new SqlCommand(deleteVillainQuery, connection);
+                sqlDeleteVillainsCommand.Parameters.AddWithValue("@villainId", value);
+                sqlDeleteVillainsCommand.ExecuteNonQuery();
+
+                Console.WriteLine($"{name} was deleted.");
+                Console.WriteLine($"{affectedRows} minions were released.");
+            }
+        }
+    }
+}
---
+++ ADO.NETExercise/07.PrintAllMinionNames/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _07.PrintAllMinionNames
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Data.SqlClient;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server = .; Integrated Security = true; Database = MinionsDB";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var minionsQuery = "SELECT Name FROM Minions";
+
+                using var minionsCommand = new SqlCommand(minionsQuery, connection);
+                using var reader = minionsCommand.ExecuteReader();
+
+                var minions = new List<string>();
+
+                while (reader.Read())
+                {
+                    minions.Add((string)reader[0]);
+                }
+
+                int counter = 0;
+
+                for (int i = 0; i < minions.Count / 2; i++)
+                {
+                    Console.WriteLine(minions[0 + counter]);
+                    Console.WriteLine(minions[minions.Count - 1 - counter]);
+                    counter++;
+                }
+
+                if (minions.Count % 2 != 0)
+                {
+                    Console.WriteLine(minions[minions.Count / 2]);
+                }
+                
+            }
+        }
+    }
+}
