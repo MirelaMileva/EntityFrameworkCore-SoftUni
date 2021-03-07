3243ab411e37a149a952a72ec889f9a8486e7726
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-03-07 15:40:21 +0000

    Add Song Above Duration Problem With LINQ

---
+++ LINQ/SongsAboveDuration/StartUp.cs
@@ -0,0 +1,109 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            var result = ExportSongsAboveDuration(context, 4);
+
+            File.WriteAllText("../../../result.text", result);
+
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            var albums = context.Producers
+                .FirstOrDefault(x => x.Id == producerId)
+                .Albums
+                .Select(x => new
+                {
+                    AlbumName = x.Name,
+                    ReleaseDate = x.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                    ProducerName = x.Producer.Name,
+                    AlbumSongs = x.Songs.Select(s => new
+                    {
+                        SongName = s.Name,
+                        Price = s.Price,
+                        Writer = s.Writer.Name
+                    }).OrderByDescending(s => s.SongName)
+                    .ThenBy(w => w.Writer)
+                    .ToList(),
+                    AlbumPrice = x.Price
+                })
+                .OrderByDescending(x => x.AlbumPrice)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var album in albums)
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}");
+                sb.AppendLine($"-ReleaseDate: {album.ReleaseDate}");
+                sb.AppendLine($"-ProducerName: {album.ProducerName}");
+                sb.AppendLine($"-Songs:");
+
+                int counter = 1;
+                foreach (var song in album.AlbumSongs)
+                {
+                    sb.AppendLine($"---#{counter++}")
+                        .AppendLine($"---SongName: {song.SongName}")
+                        .AppendLine($"---Price: {song.Price:F2}")
+                        .AppendLine($"---Writer: {song.Writer}");  
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            var songsDuration = context.Songs
+                .ToList()
+                .Where(x => x.Duration.TotalSeconds > duration)
+                .Select(x => new
+                {
+                    SongName = x.Name,
+                    PerformerFullName = x.SongPerformers
+                    .Select(x => x.Performer.FirstName + " " + x.Performer.LastName).FirstOrDefault(),
+                    Writer = x.Writer.Name,
+                    AlbumProducer = x.Album.Producer.Name,
+                    Duration = x.Duration
+                })
+                .OrderBy(s => s.SongName)
+                .ThenBy(w => w.Writer)
+                .ThenBy(p => p.PerformerFullName)
+                .ToList();
+
+            var sb = new StringBuilder();
+            int counter = 1;
+
+            foreach (var song in songsDuration)
+            {
+                sb.AppendLine($"-Song #{counter++}")
+                    .AppendLine($"---SongName: {song.SongName}")
+                    .AppendLine($"---Writer: {song.Writer}")
+                    .AppendLine($"---Performer: {song.PerformerFullName}")
+                    .AppendLine($"---AlbumProducer: {song.AlbumProducer}")
+                    .AppendLine($"---Duration: {song.Duration:c}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
