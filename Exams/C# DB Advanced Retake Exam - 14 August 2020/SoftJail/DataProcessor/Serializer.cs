404a1fa4009e390be3c8f0f6d7374e6d963bd984
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-04-03 10:12:23 +0100

    Add Exam Practise With EF Core

---
+++ Exams/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,75 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            var prisoners = context.Prisoners
+                .Where(x => ids.Contains(x.Id))
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    CellNumber = x.Cell.CellNumber,
+                    Officers = x.PrisonerOfficers.Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name,
+                    })
+                    .OrderBy(x => x.OfficerName)
+                    .ToList(),
+                    TotalOfficerSalary = decimal.Parse(x.PrisonerOfficers.Sum(x => x.Officer.Salary)
+                    .ToString("F2"))
+                })
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToList();
+
+            string json = JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+
+            return json;
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            var names = prisonersNames.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            var result = context.Prisoners
+                .Where(x => names.Contains(x.FullName))
+                .Select(x => new PrisonerViewModel
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    IncarcerationDate = x.IncarcerationDate.ToString("yyyy-MM-dd"),
+                    EncryptedMessages = x.Mails.Select(m => new EncryptedMessagesViewModel
+                    { 
+                        Description = string.Join("", m.Description.Reverse())
+                    })
+                    .ToArray(),
+                })
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(PrisonerViewModel[]), new XmlRootAttribute("Prisoners"));
+
+            using var textWriter = new StringWriter();
+
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            xmlSerializer.Serialize(textWriter, result, ns);
+
+            return textWriter.ToString();
+        }
+    }
+}
